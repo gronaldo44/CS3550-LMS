9244db302831ac89cc40be8ee8a607484327909d
gronaldo44 <98724929+gronaldo44@users.noreply.github.com>
2025-02-18 12:00:58 -0700

    A3 Phase 3

--- grades/views.py
+++ grades/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
+from . import models
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    # collect data
+    assignments = models.Assignment.objects.all()
+    if not assignments.exists():
+        raise Http404("No assignments found.")
+    
+    # call template
+    assignments_dictionary = {
+        "assignments": assignments
+    }
+    return render(request, "index.html", assignments_dictionary)
 
 def assignment(request, assignment_id):
     return render(request, "assignment.html")

2c063a9f9ebbd8c5743d33811a75b36a0c357d6b
gronaldo44 <98724929+gronaldo44@users.noreply.github.com>
2025-02-19 09:30:46 -0700

    A3 Phase 4

--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from . import models
 
 # Create your views here.
@@ -16,7 +17,21 @@ def index(request):
     return render(request, "index.html", assignments_dictionary)
 
 def assignment(request, assignment_id):
-    return render(request, "assignment.html")
+    # collect data
+    a = get_object_or_404(models.Assignment, id=assignment_id)
+    total_submissions = a.submission_set.count()
+    grader = get_object_or_404(models.User, username="g")
+    my_submissions = a.submission_set.filter(grader=grader).count()
+    total_students = models.Group.objects.get(name="Students").user_set.count()
+
+    # call template
+    submissions_dictionary = {
+        "assignment": a,
+        "total_submissions": total_submissions,
+        "my_submissions": my_submissions,
+        "total_students": total_students
+    }
+    return render(request, "assignment.html", submissions_dictionary)
 
 def submissions(request, assignment_id):
     return render(request, "submissions.html")

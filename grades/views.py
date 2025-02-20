ea75e68452d480448984bd7a33268e1d1ff4ffe6
gronaldo44 <98724929+gronaldo44@users.noreply.github.com>
2025-02-19 17:14:03 -0700

    A3 Phase 5

--- grades/views.py
+++ grades/views.py
@@ -20,8 +20,8 @@ def assignment(request, assignment_id):
     # collect data
     a = get_object_or_404(models.Assignment, id=assignment_id)
     total_submissions = a.submission_set.count()
-    grader = get_object_or_404(models.User, username="g")
-    my_submissions = a.submission_set.filter(grader=grader).count()
+    my_user = get_object_or_404(models.User, username="g")   # hard-coded login
+    my_submissions = a.submission_set.filter(grader=my_user).count()
     total_students = models.Group.objects.get(name="Students").user_set.count()
 
     # call template
@@ -34,10 +34,59 @@ def assignment(request, assignment_id):
     return render(request, "assignment.html", submissions_dictionary)
 
 def submissions(request, assignment_id):
-    return render(request, "submissions.html")
+    # collect data
+    a = get_object_or_404(models.Assignment, id=assignment_id)
+    my_user = get_object_or_404(models.User, username="g")   # hard-coded login
+    my_submissions = a.submission_set.filter(grader=my_user).order_by("author__username")
+
+    submissions_data = []
+    for s in my_submissions:
+        student = s.author.get_full_name()
+        file = s.file.url
+        score = s.score
+
+        submissions_data.append({
+            "student": student,
+            "file": file,
+            "score": score
+        })
+
+    # debug
+    #print("Submissions Data: ", submissions_data)
+    #print("first username: ", submissions_data[0]["student"])
+
+    # call template
+    submissions_dictionary = {
+        "a_title": a.title,
+        "a_points": a.points,
+        "submissions_data": submissions_data
+    }
+    return render(request, "submissions.html", submissions_dictionary)
 
 def profile(request):
-    return render(request, "profile.html")
+    # collect data
+    assignments = models.Assignment.objects.all()
+    if not assignments.exists():
+        raise Http404("No assignments found.")
+    
+    my_user = get_object_or_404(models.User, username="g")   # hard-coded login
+
+    assignments_data = []
+    for a in assignments:
+        my_submissions = a.submission_set.filter(grader=my_user).count()
+        my_graded = a.submission_set.filter(grader=my_user, score__isnull=False).count()
+
+        assignments_data.append({
+            "assignment": a,
+            "my_submissions": my_submissions,
+            "my_graded": my_graded
+        })
+
+    # Call template
+    data_dictionary = {
+        "assignments_data": assignments_data
+    }
+    return render(request, "profile.html", data_dictionary)
 
 def login_form(request):
     return render(request, "login.html")
